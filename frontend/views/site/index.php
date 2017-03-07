f2a0cf57ead8604c36435f7de97c0d0e15dbc32d
Denys Korniichuk <kornejchukdenis@gmail.com>
2017-03-07 16:37:57 +0200

    Добавил немного Lorem Ipsum'a

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,15 +4,20 @@ use yii\helpers\Html;
 ?>
 
 <div class="jumbotron">
-    <h2>Добро пожаловать на форум успешных людей!</h2>
+    <h2 class="text-center">Добро пожаловать на форум успешных людей!</h2>
     <p>Форумы подобного формата едва можно пересчитать на пальцах рук. Здесь вы можете рассказать о своих лучших практиках по организации
         прорывного бизнеса, либо задать вопросы о бизнесе, которые ранее было попросту некому задать!</p>
     <?= (Yii::$app->user->id) ? "" : "<p>Регистрация и использование форума <strong>абсолютно бесплатны</strong>, в ответ мы просим вас лишь вести информативную беседу, делая этот форум
             более полезным и интересным!</p>
-        <a class=\"btn btn-primary btn-lg\" href=\"site/sign-up\">Зарегистрироваться</a>" ?>
+       <p class='text-center'>
+        <a class=\"btn btn-primary btn-lg\" href=\"site/sign-up\">Зарегистрироваться</a>
+       </p>" ?>
+
+    <?= file_get_contents('http://loripsum.net/api/6/medium/prude') ?>
+
     <p>Проект был создан в рамках курсовой работы и работы для личного портфолио. Сайт разработан на Yii2 и лежит в открытом доступе.</p>
 
-    <p>
+    <p class="text-center">
         <a class="btn btn-primary btn-lg" href="https://github.com/DenKorn/Startup-world">Этот проект на GitHub</a>
         <a class="btn btn-primary btn-lg" href="https://vk.com/den_korn">Связаться с автором</a>
     </p>
